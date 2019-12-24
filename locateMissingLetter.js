f9186edaac370b06da57eeb9867b3bb39197a8aa
Jeffrey Mcintyre <flatlandpyhon67@hotmail.com>
2019-12-24 14:10:26 -0500

    Update locateMissingLetter.js

--- locateMissingLetter.js
+++ locateMissingLetter.js
@@ -1,27 +1,12 @@
-
-//Generate the AlphaBet
-function generateAlphabet(charCode=97){
-	if (charCode === 122) return 'z';
-	return ''.concat(String.fromCharCode(charCode))
-					.concat(generateAlphabet(++charCode));
-};
-
-function fearNotLetter(str) {
-	let alphabet = generateAlphabet().split('');
-
-	// Convert letters in alphabet to UTF-16 Codes
-	let alphabetCharCodes = str
-				.split('')
-				.map((letter,index,arry) => letter
-				.charCodeAt(0));
-	
-	let locateMissingLetter = alphabetCharCodes
-					.map((charCode,index,arry) => 
-						charCode + 1 === arry[index + 1] ? 
-						charCode : 
-						String.fromCharCode(charCode + 1));	
-	return locateMissingLetter; 
-}
-
+function fearNotLetter(str){
+	//Conver letters to charCodes
+ 	let alphabetCharCodes = str
+ 			.split('')
+ 			.map(letter => letter
+ 			.charCodeAt(0));
+ 	let result = alphabetCharCodes.find((letter,position,arr) => ++letter !== arr[++position]);
+ 	return result === alphabetCharCodes[alphabetCharCodes.length - 1] ?
+								undefined :
+								String.fromCharCode(++result);
+ };
 console.log(fearNotLetter("abce"));
-
